31380a9669a9e314b484d5582eb54e7ecdb651c7
bhavyakariwal9 <bhavyakariwal9@gmail.com>
2020-12-28 22:39:52 +0530

    changes in fragment activity and main activity

--- app/src/main/java/com/campusassist/myapplication/FragmentHome.java
+++ app/src/main/java/com/campusassist/myapplication/FragmentHome.java
@@ -1,17 +1,18 @@
 package com.campusassist.myapplication;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class FragmentHome extends Fragment {
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
 }
--- app/src/main/java/com/campusassist/myapplication/MainActivity.java
+++ app/src/main/java/com/campusassist/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.campusassist.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentHome()).commit();
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment=null;
+                    switch (item.getItemId()){
+                        case R.id.nav_profile:
+                            selectedFragment=new FragmentProfile();
+                            break;
+                        case R.id.nav_home:
+                            selectedFragment=new FragmentHome();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment=new FragmentSettings();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
 }
