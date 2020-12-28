f2777c3fda90f71c2a02f81d7957fd5aadb4de15
bhavyakariwal9 <bhavyakariwal9@gmail.com>
2020-12-28 15:06:48 +0530

    unable to add fragment superclass and methods

--- app/src/main/java/com/campusassist/myapplication/FragmentProfile.java
+++ app/src/main/java/com/campusassist/myapplication/FragmentProfile.java
@@ -1,6 +1,13 @@
 package com.campusassist.myapplication;
+//import android.support.v4.app.;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
 
-//import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 
-public class FragmentProfile {
+public class FragmentProfile extends FragmentActivity{
+
 }
