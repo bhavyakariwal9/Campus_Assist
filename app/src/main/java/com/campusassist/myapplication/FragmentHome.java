e188833a7853092d85f71248b06054d0c5a8b1a1
bhavyakariwal9 <bhavyakariwal9@gmail.com>
2020-12-28 22:22:02 +0530

    added fragment class

--- app/src/main/java/com/campusassist/myapplication/FragmentHome.java
+++ app/src/main/java/com/campusassist/myapplication/FragmentHome.java
@@ -8,10 +8,10 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 
-public class FragmentProfile extends Fragment {
+public class FragmentHome extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        return inflater.inflate(R.layout.fragment_home,container,false);
     }
 }
